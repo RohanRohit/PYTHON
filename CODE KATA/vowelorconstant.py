32a65daac1006cea278e6293c033e3fc5e274e3e
RohanRohit <36122743+RohanRohit@users.noreply.github.com>
2018-02-09 17:36:07 +0530

    Update vowelorconstant.py

--- CODE KATA/vowelorconstant.py
+++ CODE KATA/vowelorconstant.py
@@ -3,4 +3,4 @@ a=input("enter the character")
 if a == 'a' or a=='e' or a=='i' or a=='o' or a=='u' or a == 'A' or a=='E' or a=='I' or a=='O' or a=='U':   
     print("it is vowel")
 else:
-    print("it is constant")
+    print("it is consonant")
