211233d7e8552350c02173de256ae756a0e45e9c
RohanRohit <36122743+RohanRohit@users.noreply.github.com>
2018-02-09 19:33:02 +0530

    Create largestofthreenumber.py

---
+++ CODE KATA/largestofthreenumber.py
@@ -0,0 +1,9 @@
+a=int(input(""))
+b=int(input(""))
+c=int(input(""))
+if a>b and a>c:
+    print("the bigger is {}".format(a))
+elif b>a and b>c:
+    print("the bigger is {}".format(b))
+else:
+    print("the bigger is {}".format(c))
