e9dae6665b62dcbe04dc60d1588eb53fff51dd88
RohanRohit <36122743+RohanRohit@users.noreply.github.com>
2018-02-09 17:31:43 +0530

    Create vowelorconstant.py

---
+++ CODE KATA/vowelorconstant.py
@@ -0,0 +1,6 @@
+a=input("enter the character")
+#the condition will chenck both uppercase and lowercase character.
+if a == 'a' or a=='e' or a=='i' or a=='o' or a=='u' or a == 'A' or a=='E' or a=='I' or a=='O' or a=='U':   
+    print("it is vowel")
+else:
+    print("it is constant")
